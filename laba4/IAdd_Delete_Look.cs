b777bfd7d845166a54317b593f29843937408bda
KvasLover <vadimkurb22@gmail.com>
2020-11-21 21:38:51 +0300

    1) Лаба 8. Обобщённый интерфейс.

---
+++ laba4/IAdd_Delete_Look.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace laba4
+{
+    public interface IAdd_Delete_Look<T>
+    {
+        T Add8();
+        T Delete8();
+        T Look8();
+    }
+}
